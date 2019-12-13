071ace1ab97d24cc6b109323870a78bdca59301e
jdizz <jdizz@users.noreply.github.com>
2019-12-12 21:44:46 -0800

    Adding/importing keypair details

--- aws-lightsail-lab/main.tf
+++ aws-lightsail-lab/main.tf
@@ -5,3 +5,8 @@ resource "aws_lightsail_instance" "simpleStack" {
     blueprint_id        = "ubuntu_16_04_2"
     bundle_id           = "nano_2_0"
 }
+
+resource "aws_lightsail_key_pair" "jd_key_paid" {
+    name                = "importing"
+    public_key          = "${file("~/.ssh/id_rsa.pub")}"
+}
